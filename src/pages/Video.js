a067a364dc489b1500c4291f0c412b95feac47ee
Tamires Teixeira <texerathamyres@gmail.com>
2023-04-28 14:06:12 -0300

    Finalizando o projeto

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,8 +1,9 @@
 import React, {useRef, useState} from 'react'
 import VideoFooter from './components/footer/VideoFooter'
+import VideoSideBar from './components/sidebar/VideoSideBar'
 import "./Video.css"
 
-function Video() {
+function Video({likes, messages, shares, name, description, music, url }) {
 
     const videoRef = useRef(null) 
     const [play, setPlay]= useState(false)
@@ -24,10 +25,10 @@ function Video() {
                 ref={videoRef}
                 onClick={handdleStart}
                 loop
-                src='https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=d1a44acd-bef3-4b18-bafe-92fa0b26828a'>
+                src= {url}>
             </video>
-            {/* Sidebar*/}
-            <VideoFooter/>
+            <VideoSideBar likes={likes} messages={messages} shares={shares}/>
+            <VideoFooter name={name} description={description} music={music}/>
         </div>
     )
 }
